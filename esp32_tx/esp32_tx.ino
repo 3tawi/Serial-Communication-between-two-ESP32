39a841309cefd495e4f85a0638f8013f0a8b8562
3tawi <96850409+3tawi@users.noreply.github.com>
2023-01-19 13:33:22 +0100

    Update esp32_tx.ino

--- esp32_tx/esp32_tx.ino
+++ esp32_tx/esp32_tx.ino
@@ -8,51 +8,58 @@
 #include <GifDecoder.h>
 #include "FilenameFunctions.h"
 
-#define kMatrixWidth  128
-#define kMatrixHeight  64
+const uint16_t kMatrixWidth = 128;
+const uint16_t kMatrixHeight = 64; 
 #define DISPLAY_TIME_SECONDS 60000
+
+#define UpHeader 0x01
 #define DataFrame 0x02
-#define ClearHeader 0xaa
-#define UpHeader 0x08
-#define endHeader 0x04
+#define endHeader 0x03
+
 #define SD_CS 5
 #define GIF_DIRECTORY "/gifs"
+int num_files;
 Stream* mySeriel;
-uint8_t buff[5];
 
 GifDecoder<kMatrixWidth, kMatrixHeight, 12> decoder;
+const uint16_t NUM_LEDS = kMatrixWidth * kMatrixHeight + 1;
+uint8_t buff[3];
+uint8_t r[NUM_LEDS];
+uint8_t g[NUM_LEDS];
+uint8_t b[NUM_LEDS];
+
+
+// translates from x, y into an index into the LED array
+uint16_t XY(uint8_t x, uint8_t y) {
+  return (y * kMatrixWidth) + x;
+}
 
 void setDriver(Stream* s) {
   mySeriel = s;
 }
 
-void sendData(uint8_t* data)
-{
-  mySeriel->write(DataFrame);
-  mySeriel->write(data, 5);
-  mySeriel->write(endHeader);
-}
-
-
-int num_files;
-
 void screenClearCallback(void) {
-  mySeriel->write(ClearHeader);
-  mySeriel->write(endHeader);
+  delay(6000);
 }
 
 void updateScreenCallback(void) {
   mySeriel->write(UpHeader);
+  mySeriel->write(DataFrame);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    buff[0] = r[i];
+    buff[1] = g[i];
+    buff[2] = b[i];
+    mySeriel->write(buff, 3);
+  }
   mySeriel->write(endHeader);
+  //delay(30);
 }
 
 void drawPixelCallback(int16_t x, int16_t y, uint8_t red, uint8_t green, uint8_t blue) {
-  buff[0] = x;
-  buff[1] = y;
-  buff[2] = red;
-  buff[3] = green;
-  buff[4] = blue;
-  sendData(buff);
+  int xy = XY(x, y);
+  r[xy] = red;
+  g[xy] = green;
+  b[xy] = blue;
 }
 
 // Setup method runs once, when the sketch starts
@@ -94,20 +101,14 @@ void setup() {
 
 void loop() {
     static unsigned long displayStartTime_millis;
-    static int nextGIF = 1;     // we haven't loaded a GIF yet on first pass through, make sure we do that
-
-    unsigned long now = millis();
-
+    static int nextGIF = 1;   
     static int index = 0;
-    // default behavior is to play the gif for DISPLAY_TIME_SECONDS or for NUMBER_FULL_CYCLES, whichever comes first
+    
     if((millis() - displayStartTime_millis) > DISPLAY_TIME_SECONDS)
         nextGIF = 1;
-
-    if(nextGIF)
-    {
-        delay(3000);
+        
+    if(nextGIF) {
         nextGIF = 0;
-
         if (openGifFilenameByIndex(GIF_DIRECTORY, index) >= 0) {
             // Can clear screen for new animation here, but this might cause flicker with short animations
             // matrix.fillScreen(COLOR_BLACK);
@@ -134,5 +135,5 @@ void loop() {
         // There's an error with this GIF, go to the next one
         nextGIF = 1;
     }
-  //delay(3000);
+  // delay(5);
 }
