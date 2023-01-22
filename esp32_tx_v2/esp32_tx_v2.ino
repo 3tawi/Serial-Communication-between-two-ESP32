dc899476b1454c3bc80d6cfa7589b193cce7e532
3tawi <96850409+3tawi@users.noreply.github.com>
2023-01-22 19:19:26 +0100

    Update esp32_tx_v2.ino

--- esp32_tx_v2/esp32_tx_v2.ino
+++ esp32_tx_v2/esp32_tx_v2.ino
@@ -1,3 +1,8 @@
+/*
+// - Lien vidéo: https://youtu.be/AD6JaqEASAE
+// REQUIRES the following Arduino libraries:
+// - AnimatedGIF Library:  https://github.com/bitbank2/AnimatedGIF
+*/
 
 #include "MatrixCommon.h"
 #define FILESYSTEM SD
